f6b092b976312637e880acbd6cca2e20d162ed2f
Caleby Almeida <calebysoares@gmail.com>
2021-08-09 16:49:50 -0300

    test: shold return the stock endpoint names

--- node/api/src/services/AlphaVantageApiService.spec.ts
+++ node/api/src/services/AlphaVantageApiService.spec.ts
@@ -1,4 +1,4 @@
-import AlphaVantageApiService from './AlphaVantageApiService'
+import AlphaVantageApiService, { CompareActionsPrices, EarningsProjection, PriceLastAction, StockHistory } from './AlphaVantageApiService'
 
 const makeSut = (): AlphaVantageApiService => {
   const sut = AlphaVantageApiService.connection()
@@ -6,9 +6,9 @@ const makeSut = (): AlphaVantageApiService => {
 }
 
 describe('AlphaVantage Api Service', () => {
-  test('shold return the stock endpoint names', async () => {
+  test('Shold return the stock endpoint names (service stockNamesSearch)', async () => {
     const sut = makeSut()
-    const endpointNames = await sut.searchStockNames('BA')
+    const endpointNames: string[] = await sut.stockNamesSearch('BA')
     expect(endpointNames).toBeTruthy()
     expect(endpointNames.length > 0).toBe(true)
   })
