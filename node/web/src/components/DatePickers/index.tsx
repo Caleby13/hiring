46bb8c9124149b6b0d6b48b3db2989e103a47ad1
Caleby Almeida <calebysoares@gmail.com>
2021-08-09 09:24:22 -0300

    refactor: add new parameters to component date pickers

--- node/web/src/components/DatePickers/index.tsx
+++ node/web/src/components/DatePickers/index.tsx
@@ -3,16 +3,18 @@ import React, { ChangeEventHandler } from "react";
 
 interface IDataPickers {
   label: string;
+  value: string;
   onChange: ChangeEventHandler<HTMLTextAreaElement | HTMLInputElement>;
 }
 
-export const DatePickers = ({ label, onChange }: IDataPickers) => {
+export const DatePickers = ({ label, onChange, value }: IDataPickers) => {
   return (
     <form noValidate>
       <TextField
         id="date"
         label={label}
         type="date"
+        value={value}
         fullWidth
         onChange={onChange}
         InputLabelProps={{
