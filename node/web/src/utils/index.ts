9d52617447653eb1e29cab6fc9bbee3e069b1d90
Caleby Almeida <calebysoares@gmail.com>
2021-08-10 12:09:40 -0300

    feat: adicionar função de formação para ISO 8601

--- node/web/src/utils/index.ts
+++ node/web/src/utils/index.ts
@@ -1,4 +1,4 @@
-import { format, parseISO } from 'date-fns'
+import { format, parseISO, startOfDay } from 'date-fns'
 
 export const formatPrice = (value: number | string) => {
     const VALUE = Number(value)
@@ -43,6 +43,26 @@ export const formatDate = (value: string | Date) => {
 
     if (VALUE instanceof Date) {
         return format(VALUE as Date, 'dd/MM/yyyy')
+    }
+
+    return ''
+}
+
+export const formatToISOStringDate = (value: string | Date) => {
+    let VALUE: Date | null = null
+
+    if (value instanceof Date) {
+        VALUE = value
+    }
+
+    if (typeof value === 'string') {
+        VALUE = parseISO(value)
+    }
+
+    const [year, month, date] = format(VALUE as Date, 'yyyy-MM-dd').split('-')
+
+    if (VALUE instanceof Date) {
+        return startOfDay(new Date(Number(year), Number(month) - 1, Number(date)))
     }
 
     return ''
